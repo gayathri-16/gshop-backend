fd2b4ae4bc15b898afa76d0de782a9b7a0757448
gayathri-16 <gayathri.p973@gmail.com>
2023-09-13 16:07:52 +0530

    .

--- models/productModel.js
+++ models/productModel.js
@@ -31,7 +31,7 @@ const productSchema = new mongoose.Schema({
         {
             image: {
                 type: String,
-             
+                require: true
             }
         }
     ],
